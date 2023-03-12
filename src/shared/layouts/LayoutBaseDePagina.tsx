7c1c7262f5c0674197fe4fcdcdc3d9987cf7bbec
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 17:19:50 -0300

    feat: criado layout base da aplicacao

---
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -0,0 +1,49 @@
+import { Box, Icon, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { ReactNode } from 'react';
+import { useDrawerContext } from '../contexts';
+
+
+interface ILayoutBaseDePaginaProps {
+    children: React.ReactNode;
+    titulo: string;
+    toolsList?: ReactNode;
+}
+
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo, toolsList }) => {
+
+    const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+    const { toggleDrawerOpen } = useDrawerContext();
+
+    return (
+        <Box display='flex' flexDirection='column' gap={1} padding={3}>
+            <Box display='flex' alignItems='center' justifyContent='space-between' height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} gap={1}>
+                {smDown && (
+                    <IconButton onClick={toggleDrawerOpen}>
+                        <Icon>menu</Icon>
+                    </IconButton>
+                )}
+                <Typography
+                    variant={smDown ? 'h5' : mdDown ? 'h4' : 'h4'}
+                    overflow='hiden'
+                    whiteSpace='nowrap'
+                    textOverflow='ellipsis'
+                >
+                    {titulo}
+                </Typography>
+
+                {toolsList && (
+                    <Box>
+                        {toolsList}
+                    </Box>
+                )}
+            </Box>
+
+
+            <Box flex={1} overflow='auto'>
+                {children}
+            </Box>
+        </Box>
+    );
+};
