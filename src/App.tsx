cd33715d7b680ea5b9841c38a12c844a6e47e715
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 13:22:27 -0300

    feat: importado DrawerProvider a app principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { MenuLateral } from './shared/components';
-import { AppThemeProvider } from './shared/contexts';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 
 
 
 export const App = () => {
     return (
         <AppThemeProvider>
-            <BrowserRouter>
-                <MenuLateral>
-                    <AppRoutes></AppRoutes>
-                </MenuLateral>
-            </BrowserRouter>
+            <DrawerProvider>
+                <BrowserRouter>
+                    <MenuLateral>
+                        <AppRoutes></AppRoutes>
+                    </MenuLateral>
+                </BrowserRouter>
+            </DrawerProvider>
         </AppThemeProvider>
     );
 };
