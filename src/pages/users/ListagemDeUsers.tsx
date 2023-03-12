adc22cd70579b0f274aef18083a89c2e61852edf
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 20:49:11 -0300

    feat(api): testando consulta a api

--- src/pages/users/ListagemDeUsers.tsx
+++ src/pages/users/ListagemDeUsers.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ToolsList } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { UsersService } from '../../shared/services/api/users/UsersService';
 
 export const ListagemDeUsers: React.FC = () => {
 
@@ -9,13 +10,25 @@ export const ListagemDeUsers: React.FC = () => {
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
-    },[searchParams]);
+    }, [searchParams]);
+
+    useEffect(() => {
+        UsersService.getAll(1, busca)
+            .then((result) => {
+                if (result instanceof Error) {
+                    alert(result.message);
+                } else {
+                    console.log(result);
+                }
+            });
+    }, [busca]);
+
 
     return (
         <LayoutBaseDePagina
-            titulo= 'Listagem'
+            titulo='Listagem'
             tools={
-                <ToolsList 
+                <ToolsList
                     textoDaBusca={busca}
                     aoMudarTextDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
                 />
