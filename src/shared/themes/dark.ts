2016d35c47b765a11008ab51a7f2befef7d31144
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-11 23:57:05 -0300

    feat(thema/configuration): adicionado configuracao de tema escuro e claro

---
+++ src/shared/themes/dark.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const DarkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#2c83d4',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
---
+++ src/shared/themes/light.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const LightTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#2c83d4',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
