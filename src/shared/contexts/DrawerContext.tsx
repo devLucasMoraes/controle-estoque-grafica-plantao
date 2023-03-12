a5397aa4fdb1481a62e83103d6c20baa0e1418cf
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 14:00:29 -0300

    feat(context): adicionado opcoes no DrawerContext
    essas opcoes servem para adicionar de forma agil um novo botao no drawer

--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,8 +1,15 @@
 import { createContext, useCallback, useContext, useState } from 'react';
 
+interface IDrawerOptions {
+    path: string;
+    icon: string;
+    label: string;
+}
 interface IDrawerContextData {
     isDrawerOpen: boolean;
     toggleDrawerOpen: () => void;
+    drawerOptions: IDrawerOptions[];
+    setDrawerOptions: (newDrawerOptions: IDrawerOptions[]) => void;
 }
 
 const DrawerContext = createContext({} as IDrawerContextData);
@@ -19,13 +26,18 @@ interface IDrawerProviderProps {
 export const DrawerProvider: React.FC<IDrawerProviderProps> = ({children}) => {
 
     const[isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [drawerOptions, setDrawerOptions] = useState<IDrawerOptions[]>([]);
 
     const toggleDrawerOpen = useCallback(() => {
         setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
     },[]);
 
+    const handleSetDrawerOptions = useCallback((newDrawerOptions: IDrawerOptions[]) => {
+        setDrawerOptions(newDrawerOptions);
+    }, []);
+
     return (
-        <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+        <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen, drawerOptions, setDrawerOptions: handleSetDrawerOptions }}>
             {children}
         </DrawerContext.Provider>
     );
