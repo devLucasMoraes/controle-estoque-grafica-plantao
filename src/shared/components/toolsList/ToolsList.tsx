0f29ba1d5d999c8965c1be31ef59b62e0d64cc67
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 17:24:32 -0300

    feat: criado botoes de ferramentas auxiliares para listagem

---
+++ src/shared/components/toolsList/ToolsList.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Icon, TextField, useTheme } from '@mui/material';
+
+
+interface IToolsListProps {
+    textoDaBusca?: string;
+    mostrarInputBusca?: boolean;
+    textoBotaoNovo?: string;
+    mostrarBotaoNovo?: boolean;
+    aoMudarTextDeBusca?: (novoTexto: string) => void;
+    aoClicarEmNovo?: () => void;
+}
+
+export const ToolsList: React.FC<IToolsListProps> = ({
+    mostrarInputBusca = true,
+    textoDaBusca = '',
+    textoBotaoNovo = 'Criar novo',
+    mostrarBotaoNovo = true,
+    aoMudarTextDeBusca,
+    aoClicarEmNovo
+}) => {
+
+    const theme = useTheme();
+
+    return (
+        <Box
+            height={theme.spacing(5)}
+            display='flex'
+            gap={1}
+            alignItems='center'
+        >
+            {mostrarInputBusca && (
+                <TextField
+                    sx={{ width: '50%' }}
+                    size='small'
+                    variant='standard'
+                    placeholder='Pesquisar...'
+                    value={textoDaBusca}
+                    onChange={(e) => aoMudarTextDeBusca?.(e.target.value)}
+                />
+            )}
+
+            <Box flex={1} display='flex' justifyContent='end'>
+                {mostrarBotaoNovo && (
+                    <Button
+                        sx={{ px: 4 }}
+                        color='primary'
+                        variant='contained'
+                        startIcon={<Icon>add</Icon>}
+                        onClick={aoClicarEmNovo}
+                    >{textoBotaoNovo}</Button>)}
+            </Box>
+
+        </Box>
+
+    );
+};
