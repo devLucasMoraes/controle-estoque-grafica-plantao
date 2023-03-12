546535cd6877c576b08b01ddaea0cdb5e5c828a4
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 14:03:05 -0300

    feat(routes): setado array de drawerOptions
    para configurar um novo botao no drawer basta setar um novo objeto com a label, icone e parh

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,28 @@
+import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+
+
+    const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+    useEffect(() => {
+        setDrawerOptions([
+            {
+                label: 'Dashboard',
+                icon: 'dashboard',
+                path: '/dashboard'
+            }
+        ]);
+    }, []);
+
+
     return (
         <Routes>
-            <Route path="/dashboard" element={<p>Dashboard</p>}/>
-            <Route path="*" element={<Navigate to="/dashboard"/>}/>
+            <Route path="/dashboard" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Teste</Button>} />
+            <Route path="*" element={<Navigate to="/dashboard" />} />
         </Routes>
     );
 };
