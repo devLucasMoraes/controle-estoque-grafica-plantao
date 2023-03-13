3d69eb60c64806f9f0678e51462556e3fec1d8e5
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-12 21:01:20 -0300

    feat(listagemDeUsers): impedindo consultas desnecessárias com o useDebounce

--- src/pages/users/ListagemDeUsers.tsx
+++ src/pages/users/ListagemDeUsers.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ToolsList } from '../../shared/components';
+import { useDebouce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { UsersService } from '../../shared/services/api/users/UsersService';
 
@@ -8,20 +9,25 @@ export const ListagemDeUsers: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
 
+    const { debouce } = useDebouce(500);
+
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
-        UsersService.getAll(1, busca)
-            .then((result) => {
-                if (result instanceof Error) {
-                    alert(result.message);
-                } else {
-                    console.log(result);
-                }
-            });
+        debouce(() => {
+            UsersService.getAll(1, busca)
+                .then((result) => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        console.log(result);
+                    }
+                });
+        });
     }, [busca]);
+
 
 
     return (
