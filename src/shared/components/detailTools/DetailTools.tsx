a62a5609f3f0f97e06a111e3c9888e57981c4645
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-13 23:41:29 -0300

    feat(detailTools): adicionado logica

--- src/shared/components/detailTools/DetailTools.tsx
+++ src/shared/components/detailTools/DetailTools.tsx
@@ -1,6 +1,26 @@
-import { Box, Button, Divider, Icon, useTheme } from '@mui/material';
+import { Box, Button, Icon, useTheme } from '@mui/material';
 
-export const DetailTools: React.FC = () => {
+interface IDetailToolsProps {
+    mostrarBotaoSalvar?: boolean;
+    mostrarBotaoDetalhar?: boolean;
+    mostrarBotaoEditar?: boolean;
+    mostrarBotaoApagar?: boolean;
+    aoClicaeEmSalvar?: () => void;
+    aoClicaeEmDetalhar?: () => void;
+    aoClicaeEmEditar?: () => void;
+    aoClicaeEmApagar?: () => void;
+}
+
+export const DetailTools: React.FC<IDetailToolsProps> = ({
+    mostrarBotaoSalvar,
+    mostrarBotaoDetalhar,
+    mostrarBotaoEditar,
+    mostrarBotaoApagar,
+    aoClicaeEmSalvar,
+    aoClicaeEmDetalhar,
+    aoClicaeEmEditar,
+    aoClicaeEmApagar
+}) => {
 
     const theme = useTheme();
 
@@ -11,36 +31,41 @@ export const DetailTools: React.FC = () => {
             gap={1}
             alignItems='center'
         >
-            <Button
-                color='primary'
-                variant='contained'
-                startIcon={<Icon>save</Icon>}
-            >SALVAR</Button>
+            {mostrarBotaoSalvar &&
+                <Button
+                    color='primary'
+                    variant='contained'
+                    startIcon={<Icon>save</Icon>}
+                    onClick={aoClicaeEmSalvar}
+                >SALVAR</Button>
+            }
 
-            <Button
-                color='primary'
-                variant='outlined'
-                startIcon={<Icon>info</Icon>}
-            >DETALHAR</Button>
-            <Button
-                color='primary'
-                variant='outlined'
-                startIcon={<Icon>edit</Icon>}
-            >EDITAR</Button>
+            {mostrarBotaoDetalhar &&
+                <Button
+                    color='primary'
+                    variant='outlined'
+                    startIcon={<Icon>info</Icon>}
+                    onClick={aoClicaeEmDetalhar}
+                >DETALHAR</Button>
+            }
 
-            <Button
-                color='error'
-                variant='contained'
-                startIcon={<Icon>delete</Icon>}
-            >APAGAR</Button>
+            {mostrarBotaoEditar &&
+                <Button
+                    color='primary'
+                    variant='outlined'
+                    startIcon={<Icon>edit</Icon>}
+                    onClick={aoClicaeEmEditar}
+                >EDITAR</Button>
+            }
 
-            <Divider variant='middle' orientation='vertical' />
-
-            <Button
-                color='primary'
-                variant='outlined'
-                startIcon={<Icon>arrow_back</Icon>}
-            >VOLTAR</Button>
+            {mostrarBotaoApagar &&
+                <Button
+                    color='error'
+                    variant='contained'
+                    startIcon={<Icon>delete</Icon>}
+                    onClick={aoClicaeEmApagar}
+                >APAGAR</Button>
+            }
         </Box>
     );
 };
