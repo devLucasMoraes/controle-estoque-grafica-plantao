50d8350089edd9d9805faa8100279debd273dc70
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-14 22:08:16 -0300

    feat(users): setado isLoading e handle delete na tela de editar

--- src/pages/users/EditarUser.tsx
+++ src/pages/users/EditarUser.tsx
@@ -1,18 +1,48 @@
+import { LinearProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { DetailTools } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { UsersService } from '../../shared/services/api/users/UsersService';
 
 export const EditarUser: React.FC = () => {
 
     const { id = 'new' } = useParams<'id'>();
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (id !== 'new') {
+            setIsLoading(true);
+            UsersService.getById(Number(id))
+                .then(result => {
+                    setIsLoading(false);
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        navigate('/users');
+                    } else {
+                        console.log(result);
+                    }
+                });
+        }
+    }, [id]);
 
 
     const handleSave = () => {
         console.log('Save');
     };
-    const handleDelete = () => {
-        console.log('delete');
+    const handleDelete = (id: number) => {
+        if (confirm('Realmente deseja apagar?')) {
+            UsersService.deleteById(id)
+                .then(result => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        alert('Registro apagado com sucesso!');
+                        navigate('/users');
+                    }
+                });
+        }
     };
 
     return (
@@ -25,11 +55,16 @@ export const EditarUser: React.FC = () => {
                     mostrarBotaoSalvar
                     mostrarBotaoApagar={id !== 'new'}
                     mostrarBotaoDetalhar={id !== 'new'}
-                    aoClicaeEmApagar={handleDelete}
+                    aoClicaeEmApagar={() => handleDelete(Number(id))}
                     aoClicaeEmSalvar={handleSave}
                 />
             }
         >
+
+            {isLoading && (
+                <LinearProgress variant='indeterminate' />
+            )}
+
             <p>Nova {id}</p>
         </LayoutBaseDePagina>
     );
