814183e565d0a3748579323e8064033fb3ba6aa1
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-18 14:33:50 -0300

    feat(theme/configuration): modificado tema escuro

--- src/shared/themes/dark.ts
+++ src/shared/themes/dark.ts
@@ -9,5 +9,29 @@ export const DarkTheme = createTheme({
         secondary: {
             main: '#f50057',
         },
+        background: {
+            paper: '#1f1f1f',
+            default: '#3c3c3c',
+        }
     },
+    typography: {
+        h1: {
+            color: 'white'
+        },
+        h2: {
+            color: 'white'
+        },
+        h3: {
+            color: 'white'
+        },
+        h4: {
+            color: 'white'
+        },
+        h5: {
+            color: 'white'
+        },
+        h6: {
+            color: 'white'
+        }
+    }
 });
