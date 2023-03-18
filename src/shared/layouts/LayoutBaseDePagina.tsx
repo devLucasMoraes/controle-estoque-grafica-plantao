47493b3a144fab3a9e16a6008a9977bb3067d598
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-18 12:22:38 -0300

    feat(layout): retirado toggleDrawerOpen e modificacoes na responsividade
    toggleDrawerOpen agora esta no responsiveAppBar

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,7 +1,5 @@
 import { Box, Icon, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { ReactNode } from 'react';
-import { useDrawerContext } from '../contexts';
-
 
 interface ILayoutBaseDePaginaProps {
     children: React.ReactNode;
@@ -16,17 +14,10 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ childre
     const theme = useTheme();
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
     const mdDown = useMediaQuery(theme.breakpoints.down('md'));
-    const { toggleDrawerOpen } = useDrawerContext();
 
     return (
         <Box display='flex' flexDirection='column' gap={1} padding={3}>
             <Box display='flex' alignItems='center' justifyContent='space-between' height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} gap={1}>
-                {smDown && (
-                    <IconButton onClick={toggleDrawerOpen}>
-                        <Icon>menu</Icon>
-                    </IconButton>
-                )}
-
                 <Box display='flex' gap={2}>
                     {mostrarBotaoVoltar && (
                         <IconButton color='primary' size='small' sx={{ border: 1, p: 1 }} onClick={aoClicaeEmVoltar}>
@@ -35,7 +26,7 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ childre
                     )}
 
                     <Typography
-                        variant={smDown ? 'h5' : mdDown ? 'h4' : 'h4'}
+                        variant='h4'
                         overflow='hiden'
                         whiteSpace='nowrap'
                         textOverflow='ellipsis'
@@ -45,14 +36,14 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ childre
                 </Box>
 
                 {tools && (
-                    <Box>
+                    <Box  overflow='hidden'>
                         {tools}
                     </Box>
                 )}
             </Box >
 
 
-            <Box flex={1} overflow='auto'>
+            <Box  overflow='auto'>
                 {children}
             </Box>
         </Box >
