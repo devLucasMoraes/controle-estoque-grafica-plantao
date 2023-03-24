6d26483d83b7c97de0a1620361d66c1844f763cc
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-23 22:43:24 -0300

    feat(listagemDeUsers): utilizando o datagrid

--- src/pages/users/ListagemDeUsers.tsx
+++ src/pages/users/ListagemDeUsers.tsx
@@ -1,4 +1,6 @@
-import { Icon, IconButton, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
+import { Box, Icon, Paper } from '@mui/material';
+import { DataGrid, GridColDef, ptBR } from '@mui/x-data-grid';
+import { GridActionsCellItem, GridToolbar } from '@mui/x-data-grid/components';
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { ToolsList } from '../../shared/components';
@@ -11,20 +13,24 @@ export const ListagemDeUsers: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const navigate = useNavigate();
-
     const [rows, setRows] = useState<IListagemUser[]>([]);
     const [totalCount, setTotalCount] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
+    const [busca, setBusca] = useState('');
 
     const pagina = useMemo(() => {
         return Number(searchParams.get('pagina') || '1');
     }, [searchParams]);
 
 
-    const busca = useMemo(() => {
+    const buscaMemo = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: Environment.LIMITE_DE_LINHAS,
+    });
 
     const handleDelete = (id: number) => {
         if (confirm('Realmente deseja apagar?')) {
@@ -44,95 +50,146 @@ export const ListagemDeUsers: React.FC = () => {
         }
     };
 
-    const { debouce } = useDebouce(500);
+    const { debouce } = useDebouce(1000);
 
     useEffect(() => {
         setIsLoading(true);
-        debouce(() => {
-            UsersService.getAll(pagina, busca)
-                .then((result) => {
-                    setIsLoading(false);
-                    if (result instanceof Error) {
-                        alert(result.message);
-                    } else {
-                        console.log(result);
-                        setTotalCount(result.totalCount);
-                        setRows(result.data);
-                    }
-                });
-        });
-    }, [busca, pagina]);
+        UsersService.getAll(pagina, buscaMemo)
+            .then((result) => {
+                setIsLoading(false);
+                if (result instanceof Error) {
+                    alert(result.message);
+                } else {
+                    setTotalCount(result.totalCount);
+                    setRows(result.data);
+                }
+            });
+    }, [buscaMemo, pagina]);
 
+    useEffect(() => {
+        const page = paginationModel.page + 1;
+        setSearchParams({ busca: buscaMemo, pagina: page.toString() }, { replace: true });
+    }, [paginationModel]);
 
+    useEffect(() => {
+        debouce(() => setSearchParams({ busca: busca, pagina: '1' }, { replace: true }));
+    }, [busca]);
+
+    const columns = useMemo<GridColDef<IListagemUser>[]>(() => [
+        {
+            field: 'acitions',
+            headerName: '',
+            type: 'acitions',
+            minWidth: 110,
+            flex: 0.1,
+            hideable: false,
+            disableReorder: true,
+            disableColumnMenu: true,
+            disableExport: true,
+            renderCell: (params) => [
+                <GridActionsCellItem
+                    key={`delete${params.row.id}`}
+                    icon={<Icon>delete</Icon>}
+                    label="Delete"
+                    onClick={() => handleDelete(params.row.id)}
+
+                />,
+                <GridActionsCellItem
+                    key={`edit${params.row.id}`}
+                    icon={<Icon>edit</Icon>}
+                    label="Edit"
+                    onClick={() => navigate(`/users/records/edit/${params.row.id}`)}
+
+                />,
+                <GridActionsCellItem
+                    key={`edit${params.row.id}`}
+                    icon={<Icon>info</ Icon>}
+                    label="info"
+                    onClick={() => navigate(`/users/records/show/${params.row.id}`)}
+                />
+            ]
+        },
+        {
+            field: 'id',
+            headerName: 'ID',
+            flex: 0.1,
+            minWidth: 90
+        },
+        {
+            field: 'name',
+            headerName: 'Nome',
+            minWidth: 155,
+            flex: 0.3
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            minWidth: 220,
+            flex: 0.2
+        },
+        {
+            field: 'password_hash',
+            headerName: 'Senha',
+            minWidth: 155,
+            flex: 0.1
+        },
+        {
+            field: 'role',
+            headerName: 'Cargo',
+            minWidth: 155,
+            flex: 0.1
+        },
+        {
+            field: 'status',
+            headerName: 'Status',
+            minWidth: 110,
+            flex: 0.1
+        },
+        {
+            field: 'createdAt',
+            headerName: 'Criado em',
+            width: 155
+        },
+        {
+            field: 'updatedAt',
+            headerName: 'Atualizado em',
+            width: 155
+        }
+    ], [handleDelete]);
 
     return (
         <LayoutBaseDePagina
             titulo='Listagem'
+            totalCount={totalCount}
             tools={
                 <ToolsList
                     aoClicarEmNovo={() => navigate('/users/records/new')}
                     mostrarInputBusca
                     textoDaBusca={busca}
-                    aoMudarTextDeBusca={texto => setSearchParams({ busca: texto, pagina: '1' }, { replace: true })}
+                    aoMudarTextDeBusca={texto => setBusca(texto)}
                 />
             }
         >
-            <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Ações</TableCell>
-                            <TableCell>Nome</TableCell>
-                            <TableCell>Email</TableCell>
-                        </TableRow>
-                    </TableHead>
+            <Box
+                component={Paper}
+                height='99%'
+                variant='outlined'
 
-                    <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.id}>
-                                <TableCell>
-                                    <IconButton size="small" onClick={() => handleDelete(row.id)}>
-                                        <Icon>delete</Icon>
-                                    </IconButton>
-                                    <IconButton size="small" onClick={() => navigate(`/users/records/edit/${row.id}`)}>
-                                        <Icon>edit</Icon>
-                                    </IconButton>
-                                </TableCell>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.email}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
 
-                    {totalCount === 0 && !isLoading && (
-                        <caption>{Environment.LISTAGEM_VAZIA}</caption>
-                    )}
-
-                    <TableFooter>
-                        {isLoading && (
-                            <TableRow>
-                                <TableCell colSpan={3}>
-                                    <LinearProgress variant='indeterminate' />
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableFooter>
-
-                    {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
-                        <TableRow>
-                            <TableCell colSpan={3}>
-                                <Pagination
-                                    page={pagina}
-                                    onChange={(e, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
-                                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
-                                    sx={{justifyContent:'center'}}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    )}
-                    
-                </Table>
-            </TableContainer>
+            >
+                <DataGrid
+                    localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+                    rowCount={totalCount}
+                    rows={rows}
+                    columns={columns}
+                    slots={{ toolbar: GridToolbar }}
+                    loading={isLoading}
+                    paginationMode='server'
+                    paginationModel={paginationModel}
+                    onPaginationModelChange={setPaginationModel}
+                    pageSizeOptions={[Environment.LIMITE_DE_LINHAS]}
+                />
+            </Box>
         </LayoutBaseDePagina>
     );
 };
