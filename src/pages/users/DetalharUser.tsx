cc72d73f636bdcd0bff79d77fd0cb31532cfc6d4
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-23 22:46:37 -0300

    feat(detalharUsers): prototipo da tela de detalhamento de usuarios

--- src/pages/users/DetalharUser.tsx
+++ src/pages/users/DetalharUser.tsx
@@ -1,11 +1,28 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { DetailTools } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { UsersService } from '../../shared/services/api/users/UsersService';
 
 
 export const DetalharUser: React.FC = () => {
 
     const navigate = useNavigate();
+    const { id } = useParams<'id'>();
+
+
+    const handleDelete = (id: number) => {
+        if (confirm('Realmente deseja apagar?')) {
+            UsersService.deleteById(id)
+                .then(result => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        alert('Registro apagado com sucesso!');
+                        navigate('/users');
+                    }
+                });
+        }
+    };
 
     return (
         <LayoutBaseDePagina
@@ -13,9 +30,11 @@ export const DetalharUser: React.FC = () => {
             aoClicaeEmVoltar={() => navigate('/users')}
             titulo='Detalhar'
             tools={
-                <DetailTools 
+                <DetailTools
                     mostrarBotaoApagar
                     mostrarBotaoEditar
+                    aoClicaeEmApagar={() => handleDelete(Number(id))}
+                    aoClicaeEmEditar={() => navigate(`/users/records/edit/${id}`)}
                 />
             }
         >
