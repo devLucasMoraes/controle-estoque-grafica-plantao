32317475f250ffd1f728aae1e136f72b9e4975dc
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-25 17:07:40 -0300

    refactor: anexdado underLineLinkUser ao index

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './menu-lateral/MenuLateral';
 export * from './toolsList/ToolsList';
 export * from './detailTools/DetailTools';
 export * from './responsiveAppBar/ResponsiveAppBar';
+export * from './underLineLinks/UnderLineLinkUser';
