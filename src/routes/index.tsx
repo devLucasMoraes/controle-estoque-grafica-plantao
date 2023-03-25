9bace41c53c970c2bc9a36e9b16cb1b48af3ee3b
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-25 17:08:47 -0300

    feat(categorias): criado novas rotas para referenciar categorias

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Dashboard, DetalharUser, EditarUser, ListagemDeUsers } from '../pages';
+import { DetalharCategoria } from '../pages/categorias/DetalharCategorias';
+import { EditarCategoria } from '../pages/categorias/EditarCategorias';
+import { ListagemDeCategorias } from '../pages/categorias/ListagemDeCategorias';
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
@@ -19,6 +22,11 @@ export const AppRoutes = () => {
                 label: 'Usuarios',
                 icon: 'person',
                 path: '/users'
+            },
+            {
+                label: 'Categorias',
+                icon: 'category',
+                path: '/categorias'
             }
         ]);
     }, []);
@@ -31,6 +39,10 @@ export const AppRoutes = () => {
             <Route path='/users/records/edit/:id' element={<EditarUser />} />
             <Route path='/users/records/show/:id' element={<DetalharUser />} />
             <Route path='/users/records/:id' element={<EditarUser />} />
+            <Route path="/categorias" element={<ListagemDeCategorias />} />
+            <Route path='/categorias/records/edit/:id' element={<EditarCategoria />} />
+            <Route path='/categorias/records/show/:id' element={<DetalharCategoria />} />
+            <Route path='/categorias/records/:id' element={<EditarCategoria />} />
             <Route path="*" element={<Navigate to="/dashboard" />} />
         </Routes>
     );
