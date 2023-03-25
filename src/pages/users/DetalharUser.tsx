2c7b4c84d4efee25f7918370bc94165625c34e65
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-25 17:04:07 -0300

    feat(detalharUsers): tela finalizada

--- src/pages/users/DetalharUser.tsx
+++ src/pages/users/DetalharUser.tsx
@@ -1,13 +1,17 @@
+import { Box, Grid, LinearProgress, Paper, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { DetailTools } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
-import { UsersService } from '../../shared/services/api/users/UsersService';
+import { UsersService, IListagemUser } from '../../shared/services/api/users/UsersService';
 
 
 export const DetalharUser: React.FC = () => {
 
     const navigate = useNavigate();
     const { id } = useParams<'id'>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState<IListagemUser>();
 
 
     const handleDelete = (id: number) => {
@@ -24,6 +28,23 @@ export const DetalharUser: React.FC = () => {
         }
     };
 
+    useEffect(() => {
+        if (id !== 'new') {
+            setIsLoading(true);
+            UsersService.getById(Number(id))
+                .then(result => {
+                    setIsLoading(false);
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        navigate('/users');
+                    } else {
+                        console.log(result);
+                        setUser(result);
+                    }
+                });
+        }
+    }, [id]);
+
     return (
         <LayoutBaseDePagina
             mostrarBotaoVoltar
@@ -38,7 +59,134 @@ export const DetalharUser: React.FC = () => {
                 />
             }
         >
-            teste
+            <Box
+                component={Paper}
+                height='99%'
+                variant='outlined'
+            >
+                <Grid container direction='column' spacing={2} padding={4}>
+                    {isLoading && (
+                        <Grid item>
+                            <LinearProgress variant='indeterminate' />
+                        </Grid>
+                    )}
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Id
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.id}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Nome
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.name}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Email
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.email}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Senha
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.password_hash}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Cargo
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.role}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item >
+                        <Box display='flex' flexDirection='column'>
+                            <Typography
+                                component={Box}
+                                variant='caption'
+                                noWrap
+                            >
+                                status
+                            </Typography>
+                            <Typography
+                                noWrap
+                            >
+                                {user?.status}
+                            </Typography>
+                        </Box>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Criado em
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.createdAt}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography
+                            variant='caption'
+                            noWrap
+                        >
+                            Atualizado em
+                        </Typography>
+                        <Typography
+                            noWrap
+                        >
+                            {user?.updatedAt}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
         </LayoutBaseDePagina>
     );
 };
