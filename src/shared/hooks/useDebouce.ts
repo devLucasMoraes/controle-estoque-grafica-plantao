d96a18c503b904b61aa13c97919a8c425bed1824
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-29 20:49:07 -0300

    fix: corrigido erro na tipagem

--- src/shared/hooks/useDebouce.ts
+++ src/shared/hooks/useDebouce.ts
@@ -2,7 +2,7 @@ import { useCallback, useRef } from 'react';
 
 export const useDebouce = (delay = 300, notDelayInFirstTime = true) => {
 
-    const deboucing = useRef<NodeJS.Timeout>();
+    const deboucing = useRef<number>();
     const isFirstTime = useRef(notDelayInFirstTime);
 
     const debouce = useCallback((func: () => void) => {
