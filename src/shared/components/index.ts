20ae7da1d71245287254b1f65c9df7dbd55feec9
devLucasMoraes <devlucasmoraes@gmail.com>
2023-04-01 13:01:05 -0300

    feat: criado componente underLineLink para categorias
    esse componente recebe uma id e retorna o link e o nome respectivo

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -3,3 +3,4 @@ export * from './toolsList/ToolsList';
 export * from './detailTools/DetailTools';
 export * from './responsiveAppBar/ResponsiveAppBar';
 export * from './underLineLinks/UnderLineLinkUser';
+export * from './underLineLinks/UnderLineLinkCategoria';
