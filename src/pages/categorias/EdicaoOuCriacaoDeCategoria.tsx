de94fef657f45d09f9e1d8a89e62167bd4e64783
devLucasMoraes <devlucasmoraes@gmail.com>
2023-05-18 20:02:46 -0300

    refactor(categorias): organizado o codigo para melhor legibilidade

--- src/pages/categorias/EdicaoOuCriacaoDeCategoria.tsx
+++ src/pages/categorias/EdicaoOuCriacaoDeCategoria.tsx
@@ -150,6 +150,7 @@ export const EdicaoOuCriacaoDeCategoria = () => {
                         </Grid>
 
                     </Grid>
+                    
                     <Box component='section' paddingBottom={4}>
                         <CrudTools
                             mostrarBotaoSalvar
