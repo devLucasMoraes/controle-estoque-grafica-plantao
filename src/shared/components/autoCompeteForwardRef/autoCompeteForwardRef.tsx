901acd96a116b1ff8f27bde8610e6ac05e81a4e4
devLucasMoraes <devlucasmoraes@gmail.com>
2023-05-21 19:25:20 -0300

    feat(AutoCompletes): criado componentes de AutoComplete mais genericos

---
+++ src/shared/components/autoCompeteForwardRef/autoCompeteForwardRef.tsx
@@ -0,0 +1,118 @@
+/* eslint-disable react/display-name */
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import { SetStateAction, forwardRef, useCallback, useEffect, useImperativeHandle, useMemo, useState } from 'react';
+
+
+type TAutoCompleteOption = {
+    id: number;
+    label: string;
+};
+
+interface IAutoCompeteForwardRefProps {
+    isExternalLoading?: boolean;
+    initialValue?: number;
+    error?: string;
+    service: {
+        getById: (id: number) => Promise<any>;
+        getAll: (page: number, search: string) => Promise<any>;
+    }; // Tipo genérico para o serviço
+    label: string;
+    optionLabel: string; // Nome da chave do objeto de opção para exibição do rótulo
+}
+
+export interface AutoCompleteSelectedId {
+    selectedId: number;
+    setSelectedIdUndefined: () => void;
+    setComponentErrors: (error: string) => void;
+}
+
+export const AutoCompeteForwardRef = forwardRef(
+    ({ initialValue, isExternalLoading = false, error, service, label, optionLabel }: IAutoCompeteForwardRefProps, ref) => {
+        const [selectedId, setSelectedId] = useState<number | undefined>(initialValue);
+        const [errors, setErrors] = useState(error);
+        const [options, setOptions] = useState<TAutoCompleteOption[]>([]);
+        const [isLoading, setIsLoading] = useState(false);
+        const [searchTerm, setSearchTerm] = useState('');
+
+        const setComponentErrors = useCallback((error: SetStateAction<string | undefined>) => {
+            setErrors(error);
+        }, []);
+
+        const setSelectedIdUndefined = useCallback(() => {
+            setSelectedId(undefined);
+        }, []);
+
+        useImperativeHandle(ref, () => ({
+            selectedId,
+            setSelectedIdUndefined,
+            setComponentErrors,
+        }));
+
+        const selectedOption = useMemo(() => {
+            if (!selectedId) return null;
+            return options.find((option) => option.id === selectedId);
+        }, [selectedId, options]);
+
+        useEffect(() => {
+            setIsLoading(true);
+            if (selectedId) {
+                service.getById(selectedId)
+                    .then((result) => {
+                        setIsLoading(false);
+                        if (result instanceof Error) {
+                            alert(result.message);
+                        } else {
+                            setOptions([{ id: result.id, label: result[optionLabel] }]);
+                        }
+                    });
+            } else {
+                service.getAll(0, searchTerm)
+                    .then((result) => {
+                        setIsLoading(false);
+                        if (result instanceof Error) {
+                            alert(result.message);
+                        } else {
+                            setOptions(result.data.content.map((option: any) => ({ id: option.id, label: option[optionLabel] })));
+                        }
+                    });
+            }
+        }, [searchTerm, selectedId, service, optionLabel]);
+
+        const handleFocus = () => {
+            if (errors) {
+                setErrors('');
+            }
+        };
+
+        return (
+            <Autocomplete
+                openText='Abrir'
+                closeText='Fechar'
+                noOptionsText='Sem opções'
+                loadingText='Carregando...'
+                disablePortal
+                disabled={isExternalLoading}
+                value={selectedOption}
+                loading={isLoading}
+                popupIcon={isExternalLoading || isLoading ? <CircularProgress size={28} /> : undefined}
+                onInputChange={(_, newValue) => setSearchTerm(newValue)}
+                options={options}
+                onChange={(_, newValue) => {
+                    setSelectedId(newValue?.id);
+                    setSearchTerm('');
+                }}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label={label}
+                        inputRef={ref}
+                        error={!!errors}
+                        helperText={errors}
+                        onFocus={handleFocus}
+                        size='small'
+                    />
+                )}
+            />
+        );
+    }
+);
---
+++ src/shared/forms/UAutoComplete.tsx
@@ -0,0 +1,107 @@
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import { useField } from '@unform/core';
+import { useEffect, useMemo, useState } from 'react';
+
+type TUAutoCompleteOption = {
+    id: number;
+    label: string;
+};
+
+interface IUAutoCompleteProps {
+    isExternalLoading?: boolean;
+    name: string;
+    service: {
+        getById: (id: number) => Promise<any>;
+        getAll: (page: number, search: string) => Promise<any>;
+    };
+    label: string;
+    optionLabel: string;
+}
+
+export const UAutoComplete = ({
+    isExternalLoading = false,
+    name,
+    service,
+    label,
+    optionLabel,
+}: IUAutoCompleteProps) => {
+    console.log(`renderizou UAutoComplete ${label}`);
+
+    const { fieldName, clearError, error, registerField } = useField(name);
+
+    const [selectedId, setSelectedId] = useState<number | undefined>();
+    const [options, setOptions] = useState<TUAutoCompleteOption[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const autoCompleteSelectedOption = useMemo(() => {
+        if (!selectedId) return null;
+
+        const selectedOption = options.find((option) => option.id === selectedId);
+
+        if (!selectedOption) return null;
+
+        return selectedOption;
+    }, [selectedId, options]);
+
+    useEffect(() => {
+        console.log(`renderizou useEffect registerField UAutoComplete ${label}`);
+        registerField({
+            name: fieldName,
+            getValue: () => selectedId,
+            setValue: (_, newSelectedId) => setSelectedId(newSelectedId),
+        });
+    }, [registerField, fieldName, selectedId]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        console.log(`renderizou useEffect service UAutoComplete ${label}`);
+        if (selectedId) {
+            service.getById(selectedId)
+                .then((result) => {
+                    setIsLoading(false);
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        const data = [];
+                        data.push(result);
+                        setOptions(data.map((option) => ({ id: option.id, label: option[optionLabel] })));
+                    }
+                });
+        } else {
+            service.getAll(0, searchTerm)
+                .then((result) => {
+                    setIsLoading(false);
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        setOptions(result.data.content.map((option: any) => ({ id: option.id, label: option[optionLabel] })));
+                    }
+                });
+        }
+    }, [searchTerm, selectedId, service, optionLabel]);
+
+    return (
+        <Autocomplete
+            openText='Abrir'
+            closeText='Fechar'
+            noOptionsText='Sem opções'
+            loadingText='Carregando...'
+            disablePortal
+            disabled={isExternalLoading}
+            value={autoCompleteSelectedOption}
+            loading={isLoading}
+            popupIcon={isExternalLoading || isLoading ? <CircularProgress size={28} /> : undefined}
+            onInputChange={(_, newValue) => setSearchTerm(newValue)}
+            options={options}
+            onChange={(_, newValue) => {
+                setSelectedId(newValue?.id);
+                setSearchTerm('');
+                clearError();
+            }}
+            renderInput={(params) => (
+                <TextField {...params} label={label} error={!!error} helperText={error} size='small' />
+            )}
+        />
+    );
+};
